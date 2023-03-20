c4a74251f5437f74d5f2fdc791000391c6709da0
seminyoon1 <seminyoon1@gmail.com>
2023-03-20 13:20:15 -0400

    Course addition

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login'
+    'Login',
+    'Application'
 ]
 
 MIDDLEWARE = [
