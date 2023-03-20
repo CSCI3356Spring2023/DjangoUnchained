d3ff8e5d4215657373a4c5edadccc4e0bf045ddb
seminyoon1 <seminyoon1@gmail.com>
2023-03-19 22:13:45 -0400

    Changed naming

--- DjangoUnchained/DjangoUnchained/asgi.py
+++ DjangoUnchained/DjangoUnchained/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectName project.
+ASGI config for DjangoUnchained project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectName.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoUnchained.settings')
 
 application = get_asgi_application()
--- DjangoUnchained/DjangoUnchained/wsgi.py
+++ DjangoUnchained/DjangoUnchained/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectName project.
+WSGI config for DjangoUnchained project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectName.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoUnchained.settings')
 
 application = get_wsgi_application()
