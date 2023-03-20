0f1f40b9b023a171a838b599a0517859f0ae62cb
seminyoon1 <seminyoon1@gmail.com>
2023-03-19 21:55:11 -0400

    added functionality to views and courses

--- DjangoUnchained/appName/urls.py
+++ DjangoUnchained/appName/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('add_course/', views.add_course, name='add_course'),
+    path('', views.landing_page, name = "landing_page"),
+
+    path('student_login/', views.student_login, name='student_login'),
+    path('instructor_login/', views.instructor_login, name='instructor_login'),
+    path('administrator_login/', views.administrator_login, name='administrator_login'),
 ]
