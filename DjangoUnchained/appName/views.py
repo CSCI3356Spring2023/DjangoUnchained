540ec64922e81c89c049baabb19116a6ed03ca97
seminyoon1 <seminyoon1@gmail.com>
2023-03-19 20:43:25 -0400

    Finalized set up

--- DjangoUnchained/appName/views.py
+++ DjangoUnchained/appName/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
-from django.template.loader import render_to_string
-rendered = render_to_string('Add_course.html')
+
+def hello(request):
+    template = loader.get_template('Add_course.html')
+    return HttpResponse(template.render())
