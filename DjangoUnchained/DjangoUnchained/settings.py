abdc8a2f18d7a08e7fd26c9dc819239fdc96d05f
seminyoon1 <seminyoon1@gmail.com>
2023-03-21 11:12:19 -0400

    Added Login page

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+AUTH_USER_MODEL = 'Login.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'DjangoUnchained.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Login/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/"
--- README.txt
+++ README.txt
@@ -15,3 +15,6 @@ New templates for landing page, login for students instructors and adminstrators
 add database to hold, create login page (see top for following tutorial)
 Work on understanding db integration, connecting the first login to db and work on signups
 Python saving from html fields using forms, better understanding of models and usage
+
+User set up
+https://docs.djangoproject.com/en/dev/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
