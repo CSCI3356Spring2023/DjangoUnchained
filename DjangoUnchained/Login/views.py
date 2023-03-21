15ad34e25541f3b9016b9351d0ac05f6afd086e1
seminyoon1 <seminyoon1@gmail.com>
2023-03-21 11:50:08 -0400

    Connected login to instructor and admin

--- DjangoUnchained/Login/views.py
+++ DjangoUnchained/Login/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 def landing_page(request):
@@ -8,13 +10,35 @@ def landing_page(request):
     return HttpResponse(template.render())
 
 def student_login(request):
-    if request.method == "GET":
-        return render(request, "student.html")
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/website/profile/')
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {'form': form})
     
 def instructor_login(request):
-    if request.method == "GET":
-        return render(request, "instructor.html")
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/website/profile/')
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {'form': form})
     
 def administrator_login(request):
-    if request.method == "GET":
-        return render(request, "admin.html")
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/website/profile/')
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {'form': form})
+    
