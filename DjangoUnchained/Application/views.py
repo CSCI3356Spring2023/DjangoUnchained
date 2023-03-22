57db63788eec4a38ceec04788e4a35d49e2082c8
Lok Leoi <lyulelok@gmail.com>
2023-03-21 20:50:54 -0400

    fixing bug

--- DjangoUnchained/Application/views.py
+++ DjangoUnchained/Application/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
+
 # Create your views here.
 
 def student_course(request):
