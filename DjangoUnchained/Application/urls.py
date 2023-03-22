a1b2ef24c00645999042dc2b0e97de36b8d57704
Andrew Choi <choibah@bc.edu>
2023-03-21 22:31:13 -0400

    updating studentApply

--- DjangoUnchained/Application/urls.py
+++ DjangoUnchained/Application/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('add_course/', views.add_course, name='add_course'),
     path('student_course/', views.student_course, name='student_course'),
     path('admin_page/', views.admin_page, name='admin_page'),
-    path('main/', views.homepage, name="homepage")
+    path('main/', views.homepage, name="homepage"),
+    path('student_apply/', views.student_apply, name='student_apply')
 ]
