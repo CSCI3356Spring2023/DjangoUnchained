b2b5170ecf1b7833f0a62eb918cfce0a9fc5e89b
manrao3363 <manrao@bc.edu>
2023-04-03 15:24:44 -0400

    AddCourse Updates

--- DjangoUnchained/Application/admin.py
+++ DjangoUnchained/Application/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import StudentApplication
 from .models import CourseAdd
+from .models import AddCourse
 
 admin.site.register(CourseAdd)
 admin.site.register(StudentApplication)
+admin.site.register(AddCourse)
