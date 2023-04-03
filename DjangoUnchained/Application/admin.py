47f28f284b89cea3e4e2e8509635782d6e234c1d
Lok Leoi <lyulelok@gmail.com>
2023-04-03 18:50:40 -0400

    fixing adding course in django admin

--- DjangoUnchained/Application/admin.py
+++ DjangoUnchained/Application/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import StudentApplication
-from .models import CourseAdd
-from .models import AddCourse
+
+from .models import AddCourse, CourseAdd, StudentApplication
 
 admin.site.register(CourseAdd)
 admin.site.register(StudentApplication)
