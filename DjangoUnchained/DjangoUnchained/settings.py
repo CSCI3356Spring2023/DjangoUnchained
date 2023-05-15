8a166fd17751c29a861ad6a08a1e66fae1c5afa6
Andrew Choi <choibah@bc.edu>
2023-05-15 00:31:33 -0400

    Fixed settings.py

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -25,9 +25,9 @@ AUTH_USER_MODEL = 'Login.CustomUser'
 SECRET_KEY = 'django-insecure-d8p@a@277saj--m&=i@_e2a5qc@3c=*sd+$40#c=&oo6ef!#ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
