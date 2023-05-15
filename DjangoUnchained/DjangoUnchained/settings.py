53c3646e422345b1ed14386cd01f54b992f6793e
Andrew Choi <choibah@bc.edu>
2023-05-15 00:47:17 -0400

    Deployment Test

--- DjangoUnchained/DjangoUnchained/settings.py
+++ DjangoUnchained/DjangoUnchained/settings.py
@@ -25,7 +25,7 @@ AUTH_USER_MODEL = 'Login.CustomUser'
 SECRET_KEY = 'django-insecure-d8p@a@277saj--m&=i@_e2a5qc@3c=*sd+$40#c=&oo6ef!#ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
